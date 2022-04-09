c07bb5572e879bc0aeb0d0c6c115deea8fb2736d
skbiswal01 <97391127+skbiswal01@users.noreply.github.com>
2022-04-09 21:19:41 +0530

    created react app

--- sp-1-day-3_assignment3/src/components/Inventory.jsx
+++ sp-1-day-3_assignment3/src/components/Inventory.jsx
@@ -8,6 +8,27 @@ export const Inventory = () => {
   });
     // Create add and remove functions here that changes the
     // state.
+    function inventoryHandeler(str,value){
+      let m={}
+        m[str]=inv[str]+value
+      let res = {
+        ...inv,
+        ...m
+      };
+      if(res[str]<0){
+        return;
+      }
+      setInv(res);
+    }
+
+
+
+    let sum = 0;
+
+for (const value of Object.values(inv)) {
+  sum += value;
+}
+
   return (
     <div
       style={{
@@ -21,20 +42,20 @@ export const Inventory = () => {
     >
       <div className="items">
         <span>Books: </span>
-        <button className="circlularButton">+</button>
-        <button className="circlularButton">-</button>
+        <button className="circlularButton" onClick={()=>{inventoryHandeler('books',+1)}}>+</button>
+        <button className="circlularButton" onClick={()=>{inventoryHandeler('books',-1)}}>-</button>
         <span>{inv.books}</span>
       </div>
       <div className="items">
         <span>Notebooks: </span>
-        <button className="circlularButton">+</button>
-        <button className="circlularButton">-</button>
+        <button className="circlularButton" onClick={()=>{inventoryHandeler('notebooks',+1)}}>+</button>
+        <button className="circlularButton" onClick={()=>{inventoryHandeler('notebooks',-1)}}>-</button>
         <span>{inv.notebooks}</span>
       </div>
       <div className="items">
         <span>Pen: </span>
-        <button className="circlularButton">+</button>
-        <button className="circlularButton">-</button>
+        <button className="circlularButton" onClick={()=>{inventoryHandeler('pens',+1)}}>+</button>
+        <button className="circlularButton" onClick={()=>{inventoryHandeler('pens',-1)}}>-</button>
         <span>{inv.pens}</span>
       </div>
       <div className="items">
@@ -44,7 +65,7 @@ export const Inventory = () => {
         <span>{inv.inkpens}</span>
       </div>
             {/*calculate total and show it*/}
-      total: {0}
+      total: {sum}
     </div>
   );
 };
